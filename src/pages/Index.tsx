784b58c7ad15e0ce61e2da43c3e369155eeac966
nobody847 <parrotos847@gmail.com>
2025-09-16 12:42:52 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,7 +12,7 @@ const Index = () => {
   return (
     <div className="min-h-screen">
       <Navigation />
-      <main>
+      <main className="pt-16">
         <HeroSection />
         <OverviewSection />
         <EcosystemSection />
